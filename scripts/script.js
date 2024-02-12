7666bcf5de5dfb5f67cd63b69336fe2d8bfe3d36
IwuchukwuDivine <ekeneifunanya@gmail.com>
2024-02-12 12:27:49 +0100

    finished the home page

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+// for the rotating circle
+const text = document.querySelector('.text p');
+text.innerHTML = text.innerText.split('').map(
+  (char, i) => 
+    `<span style="transform:rotate(${i * 14}deg)">${char}</span>`
+).join('');
+
+
+// automatic slider for the review section
+let currentSlideIndex = 0;
+const slides = document.querySelectorAll('.slides');
+
+setInterval(() => {
+  // Hide all slides
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+
+  // Increment the current slide index
+  currentSlideIndex++;
+
+  // Reset index to 0 if it exceeds the number of slides
+  if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+  }
+
+  // Display the current slide
+  slides[currentSlideIndex].style.display = 'flex';
+
+}, 3000);
+
